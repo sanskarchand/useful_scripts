fbfa5a2841005169a41f5b27e0eb59d5d6355431
Sanskar Chand <sanskarchand@gmail.com>
2021-01-23 17:18:50 +0545

    add TOC and metadata

--- wuxia_dl.py
+++ wuxia_dl.py
@@ -9,7 +9,8 @@ import cloudscraper
 import pdfkit
 import os
 import time
-
+import subprocess
+import datetime
 #--------   constants   --------
 URL_CHAPTER = "https://www.wuxiaworld.com{chapPath}"
 URL = "https://www.wuxiaworld.com/novel/{novelName}"
@@ -46,6 +47,15 @@ def get_chapter_content(chapter_html):
 
 def make_pdf(chapter_list, name, scraper):
 
+    #generate default toc
+    if not os.path.exists("default_toc.xsl"):
+        with open('default_toc.xsl', 'w') as outfile:
+            subprocess.call(['wkhtmltopdf', '--dump-default-toc-xsl'], stdout=outfile)
+
+    toc = {
+        'xsl-style-sheet': 'toc.xsl'
+    }
+
     body = ''
     for index in range(len(chapter_list)):
         print(f"Processing chapter {index+1}...")
@@ -54,15 +64,17 @@ def make_pdf(chapter_list, name, scraper):
         content = [str(c) for c in get_chapter_content(resp.text)]
 
         # add this chapter to the body
-        body += f"<h2>{chapter.name}</h2>" + "".join(content)
+        if index == 0:
+            body += f'<h2>{chapter.name}</h2>'+ "".join(content)
+        else:
+            body += f'<h2 style="page-break-before: always;">{chapter.name}</h2>'+ "".join(content)
 
         time.sleep(POLITENESS_FACTOR)
-    '''
+
     options = {
-            'margin-bottom': '0.75in',
-            'footer-right': '[page]'
+            'margin-bottom': '20mm',
+            'footer-center': '[page]'
     }
-    '''
         
     output = """
             <!DOCTYPE html>
@@ -72,12 +84,22 @@ def make_pdf(chapter_list, name, scraper):
             </head>
             <body>
             {body}
+                <p style="page-break-before: always;">
+                    <b>Extra metadata</b>
+                    <p>Downloaded from: <a href="{url}">{name}</a></p>
+                    <p >Created on: {date} </p>
+                    <p>Created using: <a href="https://github.com/sanskarchand/useful_scripts">wuxial-dl</a></p>
+                </p>
             </body>
-            </html>""".format(body=body)      # handle unicode
+            </html>""".format(body=body, 
+                            date=str(datetime.datetime.now()),
+                            name=name,
+                            url=URL.format(novelName=name)
+                    )      # handle unicode
 
     #pdfkit.from_string(output, "{name}.pdf", options=options)
     
-    pdfkit.from_string(output, f"{name}.pdf")
+    pdfkit.from_string(output, f"{name}.pdf", toc=toc, options=options)
 
 def extract_chapters(html_page):
     chapter_list = []
@@ -107,7 +129,6 @@ def main():
         print(f"Error: Could not get main page (Status {r.status_code})")
     
     chapter_list = extract_chapters(r.text)
-    
     make_pdf(chapter_list, novelName, scraper)
 
 if __name__ == '__main__':
